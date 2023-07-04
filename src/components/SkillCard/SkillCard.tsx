aac3f8e610b148fa40e329ae7484ffb5791961aa
patrick <patdi.bautista@gmail.com>
2023-07-04 18:43:29 +0800

    Added Texture to SkillCard

--- src/components/SkillCard/SkillCard.tsx
+++ src/components/SkillCard/SkillCard.tsx
@@ -4,48 +4,59 @@ import React from "react";
 import "./component.css";
 
 /**
- * 
- * @param props 
- * @returns 
+ *
+ * @param props
+ * @returns
  */
 const SkillCard = (props: {
    name?: string;
    logoURL?: string;
    Logo?: JSX.Element;
-   alt?:string;
+   alt?: string;
    descriptions?: string[];
-   rating?:number
+   rating?: number;
 }) => {
-   const { name, logoURL, Logo, alt, descriptions } =
-      props;
+   const {
+      name,
+      logoURL,
+      Logo,
+      alt,
+      descriptions,
+   } = props;
    return (
       <div
+         id="skill-card-wrapper"
          role="listitem"
-         className="skill-card-effect w-[110px] h-[110px] sm:w-[250px] sm:h-[250px] md:w-[350px] md:h-[350px] bg-white rounded-xl shadow-md shadow-black grid grid-cols-1 sm:grid-cols-2 sm:grid-rows-custom-2 p-3 cursor-pointer sm:cursor-default"
+         className="bg-white skill-card-effect w-fit h-fit rounded-xl"
       >
-         <div className="row-span-1 col-span-1 flex flex-col items-center justify-center">
-            <h5 className="mb-1 text-base md:text-lg font-bold">
-               {name}
-            </h5>
-            {logoURL ? (
-               <img
-                  src={logoURL}
-                  alt={alt}
-                  className="w-[60px] h-[60px] md:w-[75px] md:h-[75px]"
-               />
-            ) : Logo ? (
-               Logo
-            ) : (
-               <></>
-            )}
-         </div>
-         <div className="hidden sm:block row-span-1 col-span-1"></div>
-         <div className="hidden sm:block col-span-2 p-3 pl-8 ">
-            <ul className="text-base md:text-lg font-medium list-disc">
-               {descriptions?.map(item => (
-                  <li>{item}</li>
-               ))}
-            </ul>
+         <div
+            role="listitem"
+            className="skill-card-texture w-[110px] h-[110px] sm:w-[250px] sm:h-[250px] md:w-[350px] md:h-[350px] rounded-xl shadow-md shadow-black grid grid-cols-1 sm:grid-cols-2 sm:grid-rows-custom-2 p-3 cursor-pointer sm:cursor-default"
+         >
+            <div className="row-span-1 col-span-1 flex flex-col items-center justify-center">
+               <h5 className="mb-1 text-base md:text-lg font-bold">
+                  {name}
+               </h5>
+               {logoURL ? (
+                  <img
+                     src={logoURL}
+                     alt={alt}
+                     className="w-[60px] h-[60px] md:w-[75px] md:h-[75px] drop-shadow-lg"
+                  />
+               ) : Logo ? (
+                  Logo
+               ) : (
+                  <></>
+               )}
+            </div>
+            <div className="hidden sm:block row-span-1 col-span-1"></div>
+            <div className="hidden sm:block col-span-2 p-3 pl-8 ">
+               <ul className="text-base md:text-lg font-medium list-disc">
+                  {descriptions?.map((item) => (
+                     <li>{item}</li>
+                  ))}
+               </ul>
+            </div>
          </div>
       </div>
    );
